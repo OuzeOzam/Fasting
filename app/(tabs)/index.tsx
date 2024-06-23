24e5e2dd80464287ccfbb0e2c46ecc1c71ee385c
Ouze <ossama.benjemaa@gmail.com>
2024-06-24 00:43:24 +0200

    Picker POC

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,70 +1,89 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
+import React, { useState } from 'react';
+import { View, StyleSheet, Platform, Button, TouchableWithoutFeedback, Modal, Text, Pressable, Alert } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
+import DateTimePicker, { Event } from '@react-native-community/datetimepicker';
 
 export default function HomeScreen() {
+  const [time, setTime] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+  const insets = useSafeAreaInsets();
+
+  const onChange = (event: Event, selectedDate?: Date | undefined) => {
+    const currentDate = selectedDate || time;
+    setShowPicker(Platform.OS === 'ios'); // Gestion spécifique à iOS
+    setTime(currentDate);
+  };
+
+  const showTimepicker = () => {
+    setShowPicker(true);
+  };
+
+  const hideTimepicker = () => {
+    setShowPicker(false);
+  };
+
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
+    <View style={{ flex: 1, paddingTop: insets.top }}>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
+        <ThemedText type="subtitle">Heure du dernier repas</ThemedText>
+        <View style={styles.main}>
+          <Button onPress={showTimepicker} title="Sélectionner l'heure" />
+          <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+        }}>
+        <View>
+          <View>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Show Modal</Text>
+      </Pressable>
+            <View style={styles.datepickerContainer}>
+              {showPicker && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={time}
+                  mode="datetime"
+                  is24Hour={true}
+                  display="spinner"
+                  locale="fr"
+                  onChange={onChange} // Utilisation de la fonction ajustée onChange
+                  style={{ width: '100%', alignSelf: 'center' }}
+                />
+              )}
+            </View>
+        </View>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
+  main: {},
   stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+    padding: 20,
+    alignItems: 'center',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  datepickerContainer: {
+    marginTop: 20,
+    alignItems: 'center',
   },
 });
